5ceb7452a0561cd9329db636e6de3a067698dc87
manoj kota <kota.manoj8@gmail.com>
2021-11-10 22:35:29 -0600

    add

---
+++ controllers/api.js
@@ -0,0 +1,17 @@
+// API for our resources
+
+exports.api = function (req, res) {
+
+    res.write('[');
+
+    res.write('{"resource":"valley", ');
+
+    res.write('  "verbs":["GET","PUT", "DELETE"] ');
+
+    res.write('}');
+
+    res.write(']')
+
+    res.send();
+
+};
---
+++ models/valley.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose") 
+const valleySchema = mongoose.Schema({ 
+ valley_type: String, 
+ size: String, 
+ cost: Number 
+}) 
+ 
+module.exports = mongoose.model("valley", Schema) 
